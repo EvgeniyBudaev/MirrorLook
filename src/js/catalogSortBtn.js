4aeec38bb4affc6e3b248ea2f9164b666a47360f
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-11-11 11:12:18 +0300

    update

--- src/js/catalogSortBtn.js
+++ src/js/catalogSortBtn.js
@@ -2,20 +2,20 @@ const catalogAsideFilterSelectsWrapper = document.querySelector('[data-catalog-f
 const catalogAsideButtonSort = document.querySelector('[data-catalog-btn-sort=""]')
 const catalogFilterBtn = document.querySelector('.catalog__filter-btn')
 
-
 // Отображение панели сортировки по клику на иконку
 function clickHandlerAsideButtonSort() {
   console.log('click')
-  catalogAsideFilterSelectsWrapper.classList.toggle('active')
-  catalogFilterBtn.classList.toggle('active')
-  if (catalogAsideFilterSelectsWrapper.style.maxHeight) {
-    catalogAsideFilterSelectsWrapper.style.maxHeight = null;
-  } else {
-    catalogAsideFilterSelectsWrapper.style.maxHeight = catalogAsideFilterSelectsWrapper.scrollHeight + "px";
-  }
+  // catalogAsideFilterSelectsWrapper.classList.toggle('active')
+  // catalogFilterBtn.classList.toggle('active')
+  // if (catalogAsideFilterSelectsWrapper.style.maxHeight) {
+  //   catalogAsideFilterSelectsWrapper.style.maxHeight = null;
+  // } else {
+  //   catalogAsideFilterSelectsWrapper.style.maxHeight = catalogAsideFilterSelectsWrapper.scrollHeight + "px";
+  // }
 }
 
 function showCatalogSort() {
+  console.log('catalogAsideButtonSort', catalogAsideButtonSort)
   if (catalogAsideButtonSort) catalogAsideButtonSort.addEventListener('click', clickHandlerAsideButtonSort)
 }
 
